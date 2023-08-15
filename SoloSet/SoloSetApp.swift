2bd29eae325824e240b774eeddc7a23f0e1e16f3
Pierre-Hugues Oger <phylog@gmail.com>
2023-08-15 13:45:53 +0200

    Initial Commit

---
+++ SoloSet/SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetApp.swift
+//  SoloSet
+//
+//  Created by Pierre-Hugues Oger on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
